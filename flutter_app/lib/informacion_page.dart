8aab4baf50e20c5858653d253c7a1628159fa9a4
Gael Borchardt <gaelborchardt@gmail.com>
2025-08-27 00:44:04 -0700

    RAAAAHHHH

---
+++ flutter_app/lib/informacion_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class InformacionPage extends StatelessWidget {
+  const InformacionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Información')),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Text('About')],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(mainAxisAlignment: MainAxisAlignment.center),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Text('Dart Vader'), Text('Lord of the Sith ')],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+              ),
+              child: const Text('Menú'),
+            ),
+            ListTile(
+              title: const Text('Inicio'),
+              onTap: () {
+                context.go('/');
+              },
+            ),
+            ListTile(
+              title: const Text('Juego'),
+              onTap: () {
+                context.go('/juego');
+              },
+            ),
+            ListTile(
+              title: const Text('Configuración'),
+              onTap: () {
+                context.go('/configuracion');
+              },
+            ),
+            ListTile(
+              title: const Text('Información'),
+              onTap: () {
+                context.go('/informacion');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
